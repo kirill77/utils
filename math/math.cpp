49b8aee338a104103e378ca13fe8182beea606ac
Barshan Mondal <116485454+blueee04@users.noreply.github.com>
2026-02-05 01:56:44 +0530

    Document static library implementation in math.cpp
    Added documentation for the static library implementation and its purpose.

--- math/math.cpp
+++ math/math.cpp
@@ -1,8 +1,17 @@
-// math.cpp : Defines the functions for the static library.
-//
-
-
-// TODO: This is an example of a library function
-void fnmath()
-{
-}
+/**
+ * math.cpp - Static library implementation file
+ * 
+ * This file exists because Visual Studio static library projects require
+ * at least one .cpp file to compile. All math utilities in this module
+ * are header-only for performance (inlining, templates):
+ * 
+ *   - vector.h    : Vector types (float2, float3, double3, etc.)
+ *   - matrix.h    : Matrix operations
+ *   - quat.h      : Quaternion math
+ *   - affine.h    : Affine transformations
+ *   - box.h       : Bounding box utilities
+ *   - intersections.h : Geometric intersection tests
+ * 
+ * If you need to add compiled (non-inline) math functions in the future,
+ * implement them here to avoid header bloat and reduce compile times.
+ */
